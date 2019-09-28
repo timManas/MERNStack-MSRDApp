3382c2fbcc441abc2527e5cbad7511add8ec4002
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-09-28 11:53:35 -0400

    Updates

---
+++ precache-manifest.198b1ac8f41de4e5cb46b1ffae7c479b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c885d986f4fdf80c3c64948c520d4534",
+    "url": "/MERNStack-MSRDApp/index.html"
+  },
+  {
+    "revision": "3e240ac89c8db7869706",
+    "url": "/MERNStack-MSRDApp/static/css/main.a6485052.chunk.css"
+  },
+  {
+    "revision": "56f9db00d3e0fe894c60",
+    "url": "/MERNStack-MSRDApp/static/js/2.bd0beba6.chunk.js"
+  },
+  {
+    "revision": "3e240ac89c8db7869706",
+    "url": "/MERNStack-MSRDApp/static/js/main.aa5118cc.chunk.js"
+  },
+  {
+    "revision": "2a3f822b9aac64a235fb",
+    "url": "/MERNStack-MSRDApp/static/js/runtime-main.3778d2aa.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MERNStack-MSRDApp/precache-manifest.198b1ac8f41de4e5cb46b1ffae7c479b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MERNStack-MSRDApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
